12cd15a1322a892bcc48eaa752420263eba3ad01
Leo Wan <lw3216@nyu.edu>
2022-11-29 05:53:30 -0500

    ..

--- pages/index.js
+++ pages/index.js
@@ -6,18 +6,16 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>DND Char Creater</title>
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+        Tabletop fantasy gamers can easily lose track of their characters' names, classes, stats, skills, personalities, and background stories as their journey progresses. Now worry no more with the DND Character Creator!
+        DND Character Creator is a web app that will allow users to keep track of multiple character sheets. Users can register and login. Once they're logged in, they can create or view their character sheets. For every sheet that they have, they can edit their character's names, classes, stats, skills, personalities, and background stories. The app will also calculate bonuses from items and embed them to stats. 
         </p>
       </main>
 
-      <Footer />
     </div>
   )
 }
